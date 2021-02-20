506a3448115a34feab92daee887ed736f7cfb906
Joni Turunen <joni.turunen@triplan.fi>
2021-02-20 16:23:48 +0200

    task: Implement dataframes and influxdb connection

--- quarksbar.py
+++ quarksbar.py
@@ -1,15 +1,20 @@
 from discord.ext import tasks, commands
-import time
+from influxdb import DataFrameClient
+from datetime import datetime, timezone
+import discord
 import pandas as pd
 
 
 class Rom(commands.Cog):
-    def __init__(self, quark, bot_id, monitored_server):
+    def __init__(self, quark, bot_id, monitored_server, infdb):
         self.index = 0
         self.quark = quark
         self.quarks_id = bot_id
         self.srv = monitored_server
         self.monitor.start()
+        self.infdb = infdb
+        self.influxdb_client = DataFrameClient(
+            infdb.get('host'), infdb.get('port'), infdb.get('user'), infdb.get('pass'), infdb.get('db'))
 
     def cog_unload(self):
         self.monitor.cancel()
@@ -21,12 +26,31 @@ class Rom(commands.Cog):
             if str(guild) == self.srv:
                 for member in guild.members:
                     if member.id != int(self.quarks_id):
-                        ts = pd.Timestamp(time.time(), unit='s')
-                        print(
-                            f'{ts}:: Rom monitored member {member} (ID:{member.id}) doing {member.activity} at **{self.srv}**')
+                        custom_status = True if isinstance(
+                            member.activity, discord.activity.CustomActivity) else False
+                        activity = member.activity if custom_status is not True else None
+                        activity = member.activity.name if member.activity else None
+                        rfc3339 = datetime.now(timezone.utc).astimezone()
+                        data = {'member_name': member.name, 'member_id': str(member.id),
+                                'member_activity': activity, 'member_server': self.srv, 'rfc3339': str(rfc3339)}
+                        df = pd.DataFrame(
+                            data, index=[rfc3339])
+                        # print(df)
+                        self.store_to_db(df, self.srv)
+
         self.index += 1
 
     @monitor.before_loop
     async def before_monitor(self):
         print('Rom is waiting for Quark...')
         await self.quark.wait_until_ready()
+
+    def store_to_db(self, df, server):
+        try:
+            self.influxdb_client.write_points(
+                df, measurement=server, database=self.infdb.get('db'), protocol='line')
+        except:
+            print(
+                f'Error happened while trying to write_points() with the df: \n{df}')
+            self.cog_unload()
+            exit(1)
