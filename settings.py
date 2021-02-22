64c000a998557fb6b757b6d67c1d2be7af97d69a
Joni Turunen <joni.turunen@triplan.fi>
2021-02-22 08:04:54 +0200

    task: Add new params from .env to support implemented features

--- settings.py
+++ settings.py
@@ -9,15 +9,19 @@ class Qenvs:
     def __init__(self, ):
         self.token = self.envs['DISCORD_TOKEN']
         self.server = self.envs['DISCORD_SERVER']
+        self.filters = self.envs['ACTIVITY_FILTERS']
         self.desc = self.envs['BOT_DESC']
         self.google_api_key = self.envs['GOOGLE_API_KEY']
         self.owner = int(self.envs['BOT_OWNER_ID'])
+        self.owner_name = self.envs['BOT_OWNER_NAME']
         self.bot_id = int(self.envs['BOT_ID'])
         self.influxdb_host = self.envs['INFLUXDB_HOST']
         self.influxdb_port = int(self.envs['INFLUXDB_PORT'])
         self.influxdb_user = self.envs['INFLUXDB_USER']
         self.influxdb_pass = self.envs['INFLUXDB_PASS']
         self.influxdb_name = self.envs['INFLUXDB_NAME']
+        self.monitor_interval = int(
+            self.envs['MONITORING_INTERVAL_IN_SECONDS'])
 
 
 if __name__ == '__main__':
